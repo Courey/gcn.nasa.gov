316e1204c30419eb273315a98390b8e80d6ca5dc
Courey Elliott <coureyelliott@gmail.com>
2024-07-16 10:18:05 -0400

    added pattern for internal circular links

--- app/routes/circulars.$circularId.($version)/rehypeAutolinkLiteral.ts
+++ app/routes/circulars.$circularId.($version)/rehypeAutolinkLiteral.ts
@@ -12,7 +12,8 @@ const suffixChars = /[a-zA-Z0-9/_=+]/.source
 const suffixCharsNonTerminal = /[-.~%:?]/.source
 const prefix = /https?:\/\//.source
 const terminal = /\s|$/.source
-const regexp = new RegExp(
+const gcnRegexp = /GCN\s(?:#?\d+|Circular\s\d+)/gi
+const urlRegexp = new RegExp(
   `${prefix}(?:${suffixChars}|(?:${suffixCharsNonTerminal})(?!${terminal}))+`,
   'g'
 )
@@ -21,15 +22,26 @@ function autolinkLiteral(tree: Parameters<typeof findAndReplace>[0]) {
   findAndReplace(
     tree,
     [
-      regexp,
-      (href: string) =>
-        h('a', { rel: 'external noopener', href, target: '_blank' }, href),
+      [
+        urlRegexp,
+        (href) =>
+          h('a', { rel: 'external noopener', href, target: '_blank' }, href),
+      ],
+      [
+        gcnRegexp,
+        (match) => {
+          const id = match.match(/\d+[a-zA-Z]?/)[0]
+          const href = `/circulars/${id}`
+          return h('a', { href }, match)
+        },
+      ],
     ],
     { ignore: ['data'] }
   )
   return tree
 }
 
+export { autolinkLiteral }
 /**
  * Remark plugin to transform strings that look like URLs to hyperlinks.
  * This approximates the "autolink literal" functionality of
